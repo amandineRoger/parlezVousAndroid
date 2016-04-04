44dac1707f77d586f805bb185dc51c3195ff7590
amandineRoger <aroger@excilys.com>
2016-04-04 15:00:02 +0200

    ex4

--- app/src/main/java/com/example/excilys/exercice1/MainActivity.java
+++ app/src/main/java/com/example/excilys/exercice1/MainActivity.java
@@ -3,17 +3,39 @@ package com.example.excilys.exercice1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = MainActivity.class.getSimpleName();
 
+    private EditText usernameField;
+    private EditText passwordField;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate !");
+
+        usernameField = (EditText) findViewById(R.id.username);
+
     }
+
+    public void sendListener (View view){
+        Toast.makeText(this, "Toast !", Toast.LENGTH_SHORT).show();
+    }
+
+    public void viderListener(View view){
+        usernameField = (EditText) findViewById(R.id.username);
+        usernameField.setText("");
+        passwordField = (EditText) findViewById(R.id.password);
+        passwordField.setText("");
+    }
+
 
     @Override
     protected void onDestroy() {
